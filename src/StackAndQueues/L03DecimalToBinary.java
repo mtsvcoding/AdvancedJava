739e316cd77d88d1cc7253dc35f01ad4a17d985f
Java Advanced January 2022 <mts.racing@gmail.com>
2023-02-15 02:41:15 +0200

    modified structure of archive

--- src/StackAndQueues/L03DecimalToBinary.java
+++ src/StackAndQueues/L03DecimalToBinary.java
@@ -1,12 +1,29 @@
 package StackAndQueues;
 
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class L03DecimalToBinary {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int inputNum = Integer.parseInt(scanner.nextLine());
 
+        ArrayDeque <Integer> stack = new ArrayDeque<>();
+
+        if (inputNum == 0){
+            System.out.println(inputNum);
+            return;
+        }else {
+            while (inputNum != 0){
+                stack.push(inputNum % 2);
+                inputNum /= 2;
+            }
+        }
+
+        while (!stack.isEmpty()){
+            System.out.print(stack.pop());
+        }
     }
 }
