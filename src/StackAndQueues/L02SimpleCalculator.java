43797f9599adc2186e1c3ee16ae8ebeefe15a8df
Java Advanced January 2022 <mts.racing@gmail.com>
2023-02-15 02:24:57 +0200

    modified structure of archive

--- src/StackAndQueues/L02SimpleCalculator.java
+++ src/StackAndQueues/L02SimpleCalculator.java
@@ -1,5 +1,6 @@
 package StackAndQueues;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class L02SimpleCalculator {
@@ -7,6 +8,29 @@ public class L02SimpleCalculator {
 
         Scanner scanner = new Scanner(System.in);
 
+        String [] input = scanner.nextLine().split(" ");
+        ArrayDeque <String> stack = new ArrayDeque<>();
 
+        for (int i = input.length - 1; i >= 0 ; i--) {
+            stack.push(input[i]);
+        }
+
+        while (stack.size() > 1){
+            int firstNum = Integer.parseInt(stack.pop());
+            String operator = stack.pop();
+            int secondNum = Integer.parseInt(stack.pop());
+
+            switch (operator){
+                case "+":
+                    stack.push(String.valueOf(firstNum + secondNum));
+                    break;
+                case "-":
+                    stack.push(String.valueOf(firstNum - secondNum));
+                    break;
+            }
+        }
+
+
+        System.out.println(stack.pop());
     }
 }
---
+++ src/StackAndQueues/L03DecimalToBinary.java
@@ -0,0 +1,12 @@
+package StackAndQueues;
+
+
+import java.util.Scanner;
+
+public class L03DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
